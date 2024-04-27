539ccd78037d5f58727de6dc487e739b049346d2
Jim White <jim@ifcx.org>
2024-04-27 16:25:28 -0700

    Fix freeze_support() not called error.

--- run_preprocess_dataset.py
+++ run_preprocess_dataset.py
@@ -1,7 +1,10 @@
+from multiprocessing import freeze_support
 from compute_indicators_labels_lib import preprocess
 from config import RUN as run_conf
 
 # preprocess coins timeseries and buildup the dataset with features and different
 # labelings for differenti forward and backward windows combination
 
-preprocess(run_conf)
+if __name__ == "__main__":   
+    freeze_support()
+    preprocess(run_conf)
